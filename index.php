efc295e8258298f5a54ce0419ae4da6b1f851a5f
mihaipaulorban <mihaipaulorban@gmail.com>
2024-01-15 14:24:05 +0100

    feat: funzione per inserire i generi

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 include 'movie.php';
 
 // Aggiungo alla classe il film con i valori
-$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
-$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994);
+$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, ["Drammatico", "Crime"]);
+$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, ["Crime", "Commedia", "Drammatico"]);
 
 echo $movie1->getMovieInfo();
 echo "<br>";
--- movie.php
+++ movie.php
@@ -6,18 +6,22 @@ class Movie
     public $title;
     public $director;
     public $year;
+    public $genres;
 
     // Definisco il construct per le variabili
-    function __construct($title, $director, $year)
+    function __construct($title, $director, $year, $genres = [])
     {
         $this->title = $title;
         $this->director = $director;
         $this->year = $year;
+        $this->genres = $genres;
     }
 
     // Funzione per il return del film
     public function getMovieInfo()
     {
-        return "{$this->title}, diretto da {$this->director}, anno di uscita {$this->year}";
+        // Uso implode per unire gli elementi dell'array alla stringa di info sul film
+        $genresString = implode(", ", $this->genres);
+        return "{$this->title}, diretto da {$this->director}, anno di uscita: {$this->year}, generi: {$genresString}";
     }
 }
