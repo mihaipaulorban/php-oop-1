531cca6a016076d41ab422fae576abb14f3f35c7
mihaipaulorban <mihaipaulorban@gmail.com>
2024-01-15 13:57:43 +0100

    feat: contructor e risultato dei film a schermo

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 include 'movie.php';
+
+// Aggiungo alla classe il film con i valori
+$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
+$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994);
+
+echo $movie1->getMovieInfo();
+echo "<br>";
+echo $movie2->getMovieInfo();
--- movie.php
+++ movie.php
@@ -1,7 +1,23 @@
 <?php
+// Classe per i Film
 class Movie
 {
+    // Variabili di istanza per ogni film
     public $title;
     public $director;
     public $year;
+
+    // Definisco il construct per le variabili
+    function __construct($title, $director, $year)
+    {
+        $this->title = $title;
+        $this->director = $director;
+        $this->year = $year;
+    }
+
+    // Funzione per il return del film
+    public function getMovieInfo()
+    {
+        return "{$this->title}, diretto da {$this->director}, anno di uscita {$this->year}";
+    }
 }
